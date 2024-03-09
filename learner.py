92129443be53f4780186e15cfd48afdc6a538f52
unknown <atchayah2004@gmail.com>
2024-03-09 19:46:04 +0530

    updating

--- learner.py
+++ learner.py
@@ -1,2 +1,35 @@
 print("Hello learner Welcome")
-#lang=input("Please select your language ")
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class Website:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, username, password):
+        self.users[username] = password
+
+    def login(self, username, password):
+        if username in self.users and self.users[username] == password:
+            return True
+        else:
+            return False
+
+# Example usage
+website = Website()
+website.add_user('user1', 'password1')
+website.add_user('user2', 'password2')
+
+def main():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    if website.login(username, password):
+        print("Login successful")
+    else:
+        print("Invalid username or password")
+
+if __name__ == "__main__":
+    main()
