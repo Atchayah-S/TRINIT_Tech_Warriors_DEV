87cdc16bda7fcdaf707f36da606ffc15ec551412
unknown <atchayah2004@gmail.com>
2024-03-10 06:26:12 +0530

    updating

--- learner.py
+++ learner.py
@@ -1,8 +1,9 @@
 import hashlib
+import os
 def signup():
-    email = input("Enter email address: ")
-    pwd = input("Enter password: ")
-    conf_pwd = input("Confirm password: ")
+    email = input("")
+    pwd = input()
+    conf_pwd = input()
     if conf_pwd == pwd:
         enc = conf_pwd.encode()
         hash1 = hashlib.md5(enc).hexdigest()
@@ -10,33 +11,31 @@ def signup():
              f.write(email + "\n")
              f.write(hash1)
         f.close()
-        print("You have registered successfully!")
+        login(email,pwd)
     else:
         print("Password is not same as above! \n")
-def login():
-    print("e-mail :")
-    email = input()
-    pwd = input("Enter password: ")
+def login(email,pwd):
+   
     auth = pwd.encode()
     auth_hash = hashlib.md5(auth).hexdigest()
     with open("credentials.txt", "r") as f:
+        
         stored_email, stored_pwd = f.read().split("\n")
     f.close()
     if email == stored_email and auth_hash == stored_pwd:
          print("Login successfully")
+         print("Welcome Learner! \nMake a move to upgrade yourself with current world")
     else:
          print("Login failed! \n")
 while 1:
-    print("********** Login System **********")
-    print("1.Signup")
-    print("2.Login")
-    print("3.Exit")
-    ch = int(input("Enter your choice: "))
-    if ch == 1:
+  #  print("********** Login System **********")
+    ch=input()
+    if ch == "signup":
         signup()
-    elif ch == 2:
-        login()
-    elif ch == 3:
+    elif ch == "login":
+        login(input(),input())
+        break
+    elif ch == "exit":
         break
     else:
         print("Wrong Choice!")
--- tutor.py
+++ tutor.py
@@ -1,1 +1,41 @@
-print("Tutor's Page")
+import hashlib
+import os
+def signup():
+    email = input("")
+    pwd = input()
+    conf_pwd = input()
+    if conf_pwd == pwd:
+        enc = conf_pwd.encode()
+        hash1 = hashlib.md5(enc).hexdigest()
+        with open("credentials.txt", "w") as f:
+             f.write(email + "\n")
+             f.write(hash1)
+        f.close()
+        login(email,pwd)
+    else:
+        print("Password is not same as above! \n")
+def login(email,pwd):
+   
+    auth = pwd.encode()
+    auth_hash = hashlib.md5(auth).hexdigest()
+    with open("credentials.txt", "r") as f:
+        
+        stored_email, stored_pwd = f.read().split("\n")
+    f.close()
+    if email == stored_email and auth_hash == stored_pwd:
+         print("Login successfully")
+         print("Welcome Tutor! \nSharing is a good thing especially when comes to knowledge")
+    else:
+         print("Login failed! \n")
+while 1:
+  #  print("********** Login System **********")
+    ch=input()
+    if ch == "signup":
+        signup()
+    elif ch == "login":
+        login(input(),input())
+        break
+    elif ch == "exit":
+        break
+    else:
+        print("Wrong Choice!")
